d5b5a3e60e1dc5bcb0776839e831ed81444b328e
Han Seob Kim <khseob0715@gmail.com>
2018-12-30 19:25:51 +0900

    view process

--- Assets/02.Scripts/MoveCtrl.cs
+++ Assets/02.Scripts/MoveCtrl.cs
@@ -17,27 +17,39 @@ public class MoveCtrl : MonoBehaviour {
     // 회전 시 회전 속도를 조절할 계수 
     public float damping = 3.0f;
 
-    public Transform tr;
+    private Transform tr;
+    private Transform camTr;
+    private CharacterController cc;
+
     // 웨이포인트를 저장할 배열 
     private Transform[] points;
     // 다음에 이동할 위치 인덱스 변수 
-    private int nextIdx = 1; 
+    private int nextIdx = 1;
+
+    public static bool isStopped = false;
 
 	// Use this for initialization
 	void Start () {
         tr = GetComponent<Transform>();
+        camTr = Camera.main.GetComponent<Transform>();
+        cc = GetComponent<CharacterController>();
+
         // WayPointGroup 게임 오브젝트 아래에 있는 모든 Point의 Transform 컴포넌트를 추출
         points = GameObject.Find("WayPointGroup").GetComponentsInChildren<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (isStopped) return; 
+
         switch (moveType)
         {
             case MoveType.WAY_POINT:
                 MoveWayPoint();
                 break;
             case MoveType.LOOK_AT:
+                MoveLookAt();
                 break;
             case MoveType.DAYDREAM:
                 break;
@@ -56,6 +68,14 @@ public class MoveCtrl : MonoBehaviour {
 
         // 전진 방향으로 이동 처리 
         tr.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    void MoveLookAt()
+    {
+        // 메인카메라가 바라보는 방향
+        Vector3 dir = camTr.TransformDirection(Vector3.forward);
+        // dir 벡터의 방향으로 초당 speed 만큼씩 이동
+        cc.SimpleMove(dir * speed);
     }
 
     private void OnTriggerEnter(Collider coll)
