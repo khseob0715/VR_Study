a0d1b12735763dcf2aa031631abb023dc477e3d4
Han Seob Kim <khseob0715@gmail.com>
2018-12-30 14:12:09 +0900

    way point move processing

---
+++ Assets/02.Scripts/MoveCtrl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCtrl : MonoBehaviour {
+    public enum MoveType
+    {
+        WAY_POINT,
+        LOOK_AT,
+        DAYDREAM
+    }
+
+    // 이동방식
+    public MoveType moveType = MoveType.WAY_POINT;
+    // 이동 속도
+    public float speed = 1.0f;
+    // 회전 시 회전 속도를 조절할 계수 
+    public float damping = 3.0f;
+
+    public Transform tr;
+    // 웨이포인트를 저장할 배열 
+    private Transform[] points;
+    // 다음에 이동할 위치 인덱스 변수 
+    private int nextIdx = 1; 
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+        // WayPointGroup 게임 오브젝트 아래에 있는 모든 Point의 Transform 컴포넌트를 추출
+        points = GameObject.Find("WayPointGroup").GetComponentsInChildren<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (moveType)
+        {
+            case MoveType.WAY_POINT:
+                MoveWayPoint();
+                break;
+            case MoveType.LOOK_AT:
+                break;
+            case MoveType.DAYDREAM:
+                break;
+        }
+
+	}
+
+    void MoveWayPoint()
+    {
+        // 현재 위치에서 다음 웨이포인트를 바라보는 벡터를 계산
+        Vector3 direction = points[nextIdx].position - tr.position;
+        // 산출된 벡터의 회전 각도를 쿼터니언 타입으로 산출 
+        Quaternion rot = Quaternion.LookRotation(direction);
+        // 현재 각도에서 회전해야 할 각도까지 부드럽게 회전 처리 
+        tr.rotation = Quaternion.Slerp(tr.rotation, rot, Time.deltaTime * damping);
+
+        // 전진 방향으로 이동 처리 
+        tr.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        // 웨이포인트(point 게임오브젝트)에 충돌 여부 판단 
+        if (coll.CompareTag("WAY_POINT"))
+        {
+            // 맨 마지막 웨이포인트에 도달 했을 경우 처음 인덱스로 변경
+            nextIdx = (++nextIdx >= points.Length) ? 1 : nextIdx;
+        }
+    }
+}
---
+++ Assets/02.Scripts/WayPointTrack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointTrack : MonoBehaviour {
+    public Color LineColor = Color.yellow;
+    private Transform[] points;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        // 라인 색상 지정
+        Gizmos.color = LineColor;
+        // WayPointGroup 게임 오브젝트 아래에 있는 모든 Point 게임 오브젝트 추출
+        points = GetComponentsInChildren<Transform>();
+
+        int nextIdx = 1;
+
+        Vector3 currPos = points[nextIdx].position;
+        Vector3 nextPos;
+
+        //Point 게임오브젝트를 순회하면서 라인을 그림. 
+        for(int i = 0; i <= points.Length; i++)
+        {
+            // 마지막 Point일 경우 첫 번째 point로 지정
+            nextPos = (++nextIdx >= points.Length) ? points[1].position : points[nextIdx].position;
+            // 시작 위치에서 종료위치까지 라인을 그림. 
+            Gizmos.DrawLine(currPos, nextPos);
+
+            currPos = nextPos;
+        }
+    }
+}
